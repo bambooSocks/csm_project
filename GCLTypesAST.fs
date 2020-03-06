1653f4680a253bfa45440cac4a588e44e43d40de
matej <majtan.matej@gmail.com>
2020-03-06 08:45:54 +0100

    added types file

---
+++ GCLTypesAST.fs
@@ -0,0 +1,14 @@
+module GCLTypesAST
+
+type expr =
+  | Num of float
+  | TimesExpr of (expr * expr)
+  | DivExpr of (expr * expr)
+  | PlusExpr of (expr * expr)
+  | MinusExpr of (expr * expr)
+  | PowExpr of (expr * expr)
+  | UPlusExpr of (expr)
+  | UMinusExpr of (expr)
+  | SqrtExpr of (expr)
+  | LogExpr of (expr)
+  | LnExpr of (expr)
