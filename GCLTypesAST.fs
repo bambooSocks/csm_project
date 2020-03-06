4e48132511dc4d2c4abafeb677ca4b8f01e26698
Mihaela Nistor <nistor.mihaela5@gmai.com>
2020-03-06 09:36:24 +0100

    Finished type declarations

--- GCLTypesAST.fs
+++ GCLTypesAST.fs
@@ -1,14 +1,41 @@
 module GCLTypesAST
 
-type expr =
-  | Num of float
-  | TimesExpr of (expr * expr)
-  | DivExpr of (expr * expr)
-  | PlusExpr of (expr * expr)
-  | MinusExpr of (expr * expr)
-  | PowExpr of (expr * expr)
-  | UPlusExpr of (expr)
-  | UMinusExpr of (expr)
-  | SqrtExpr of (expr)
-  | LogExpr of (expr)
-  | LnExpr of (expr)
+type AExp = 
+      | Num of int 
+      | Var of string
+      | Array of (string*AExp)
+      | Plus of (AExp*AExp) 
+      | Minus of (AExp*AExp)
+      | Mul of (AExp*AExp)
+      | Div of (AExp*AExp)
+      | Pow of (AExp*AExp)
+
+type BExp =
+      | TExp
+      | FExp
+      | AndExp of (BExp*BExp)
+      | OrExp of (BExp*BExp)
+      | ShortAndExp of (BExp*BExp)
+      | ShortOrExp of (BExp*BExp)
+      | NotExp of BExp
+      | EqExp of (BExp*BExp)
+      | NotEqExp of (BExp*BExp)
+      | GreaterExp of (BExp*BExp)
+      | GreaterEqExp of (BExp*BExp)
+      | LessExp of (BExp*BExp)
+      | LessEqExp of (BExp*BExp)
+
+type CExp = 
+      | Assignment of (string*AExp)
+      | ArrayAssignment of (string*AExp*AExp)
+      | SkipExp
+      | CExpSeq of (CExp*CExp)
+      | IfExp of GCExp
+      | DoExp of GCExp
+
+   and 
+
+type GCExp = 
+      | GC of (BExp*CExp)
+      | GCSeq of (GCExp*GCExp)
+        
