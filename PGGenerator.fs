7358c62d6c3e842c2b1b2d5f63a4604bd00d7787
Mihaela Nistor <nistor.mihaela5@gmai.com>
2020-03-23 13:25:49 +0100

    PGGenerator cleanup

--- PGGenerator.fs
+++ PGGenerator.fs
@@ -1,22 +1,21 @@
 module PGGenerator
 
-let mutable nodeNumbers = Set.empty //set of node numbers
+let mutable nodes = Set.empty //set of node numbers
 
 type NodeType = | EndNode
                 | Node of int
 
 let rec newNode node = 
     match node with
-    | EndNode -> failwith("cannot append a node")
-    | Node n -> if Set.contains (n+1) nodeNumbers then
-                    newNode (Node(n+1))
-                else
-                    nodeNumbers <- Set.add (n+1) nodeNumbers 
-                    Node(n+1)
+    | EndNode                              -> failwith("cannot append a node")
+    | Node n when Set.contains (n+1) nodes -> newNode (Node(n+1))
+    | Node n                               -> nodes <- Set.add (n+1) nodes
+                                              Node(n+1)
+                
 
-let rec collectGuards = function
+let rec colGuards = function
     | GC (guard, _)    -> Set.ofList [guard]
-    | GCSeq (gc1, gc2) -> Set.union (collectGuards gc1) (collectGuards gc2)
+    | GCSeq (gc1, gc2) -> Set.union (colGuards gc1) (colGuards gc2)
 
 let rec doneGen = function
     | GC (guard, _)    -> NotExp guard
@@ -26,30 +25,32 @@ let rec detGuard = function
     | []    -> FExp
     | b::bs -> ShortOrExp (b, (detGuard bs))
 
-let rec edgesC node1 node2 output is_det = function
-    | Assignment(var, exp) -> Set.add (node1, C(Assignment(var, exp)), node2) output
-    | CExpSeq(cmd1, cmd2) -> let q = newNode node1
-                             let intermediate = edgesC node1 q output is_det cmd1
-                             edgesC q node2 intermediate is_det cmd2
-    | SkipExp -> Set.add(node1,C(SkipExp),node2) output
-    | ArrayAssignment(var,ind,exp) -> Set.add(node1,C(ArrayAssignment(var,ind,exp)),node2) output
-    | IfExp (gexp) -> let all_branches = collectGuards gexp
-                      edgesG node1 node2 output is_det all_branches gexp
-    | DoExp (gexp) -> let doneExp = doneGen gexp
-                      let all_branches = collectGuards gexp
-                      let intermediate = edgesG node1 node1 output is_det all_branches gexp 
-                      Set.add (node1, B(doneExp) ,node2) intermediate
+let rec edgesC n1 n2 output isDet = function
+    | Assignment(var, exp)          -> Set.add (n1, C(Assignment(var, exp)), n2) output
+    | CExpSeq(cmd1, cmd2)           -> let q = newNode n1
+                                       let aux = edgesC n1 q output isDet cmd1
+                                       edgesC q n2 aux isDet cmd2
+
+    | SkipExp                       -> Set.add(n1,C(SkipExp),n2) output
+    | ArrayAssignment(var,ind,exp)  -> Set.add(n1,C(ArrayAssignment(var,ind,exp)),n2) output
+    | IfExp (guard)                 -> let allBranches = colGuards guard
+                                       edgesG n1 n2 output isDet allBranches guard
+
+    | DoExp (guard)                 -> let doneExp = doneGen guard
+                                       let allBranches = colGuards guard
+                                       let aux = edgesG n1 n1 output isDet allBranches guard 
+                                       Set.add (n1, B(doneExp) ,n2) aux
                    
-and edgesG node1 node2 output is_det d = function
-    | GC (guard, cmd)  -> let q = newNode node1
-                          if is_det then
-                              let set_wo_guard = Set.difference d (Set.ofList [guard])
-                              let dexp = detGuard (Set.toList set_wo_guard)
-                              let s = Set.add (node1, B(ShortAndExp(guard, (NotExp dexp))), q) output
-                              edgesC q node2 s is_det cmd
-                          else
-                              let s = Set.add (node1, B(guard), q) output
-                              edgesC q node2 s is_det cmd
-    | GCSeq (gc1, gc2) -> let e1 = edgesG node1 node2 output is_det d gc1 
-                          edgesG node1 node2 e1 is_det d gc2
+and edgesG n1 n2 output isDet d = function
+    | GC (guard, cmd) when isDet -> let q = newNode n1  
+                                    let setWoGuards = Set.difference d (Set.ofList [guard])
+                                    let dexp = detGuard (Set.toList setWoGuards) 
+                                    let s = Set.add (n1, B(ShortAndExp(guard, (NotExp dexp))), q) output
+                                    edgesC q n2 s isDet cmd
+
+    | GC (guard, cmd)            -> let s = Set.add (n1, B(guard), q) output
+                                    edgesC q n2 s isDet cmd                      
+                              
+    | GCSeq (gc1, gc2)           -> let e1 = edgesG n1 n2 output isDet d gc1 
+                                    edgesG n1 n2 e1 isDet d gc2
                           
